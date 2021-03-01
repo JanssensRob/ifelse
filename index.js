ec7802118ecd7e5ec1cd5baf0364098f938e2325
Rob <robjanssens93@hotmail.com>
2021-03-01 16:56:18 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,42 @@
+const age = 20;
+
+
+if (age >= 18) {
+    console.log("Persoon mag naar binnen");
+}
+else {
+    console.log("Persoon mag niet naar binnen")
+};
+
+//is gelukt
+
+const isFemale = "male";
+
+if (isFemale == "female") {
+    console.log("Dit is een vrouw");
+}
+else {
+    console.log("Persoon is geen vrouw");
+
+}
+
+//is gelukt
+
+const DriverStatus = 'bob';
+
+if (DriverStatus == "bob") {
+    console.log("Bestuurder mag autorijden");
+} else {
+    console.log("Bestuurder mag niet rijden");
+}
+
+const Driverstatus = 'nietbob';
+
+if (DriverStatus == "nietbob") {
+    console.log("Bestuurder mag autorijden");
+} else {
+    console.log("Bestuurder mag niet rijden");
+}
+
+//is gelukt
+
